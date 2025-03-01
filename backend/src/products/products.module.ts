abd27fe588f92fdeec54900846c8be1ee95db9a6
Ivan Ticona <doriansamt@gmail.com>
2025-03-01 17:28:20 -0400

    Products Module

---
+++ backend/src/products/products.module.ts
@@ -0,0 +1,17 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+import { Module } from '@nestjs/common';
+import { ProductsController } from './products.controller';
+import { ProductsService } from './products.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Product, ProductSchema } from './products.schema';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Product.name, schema: ProductSchema }]),
+  ],
+  controllers: [ProductsController],
+  providers: [ProductsService],
+})
+export class ProductsModule {}
