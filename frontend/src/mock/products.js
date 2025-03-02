1d7972b2cf3e9e7da02157dbb80e6fcdc69386de
Ivan Ticona <doriansamt@gmail.com>
2025-03-01 21:48:56 -0400

    Updated

---
+++ frontend/src/mock/products.js
@@ -0,0 +1,62 @@
+export const productos = [
+  {
+    id: 1,
+    nombre: "Camiseta Deportiva",
+    descripcion:
+      "Camiseta transpirable para actividades deportivas, confeccionada en material 100% algodón.",
+    precio: 19.99,
+    categoria: "Ropa",
+    imagen:
+      "https://assets-es.imgfoot.com/media/cache/800x800/fc-barcelone-home-2013-2014.jpg",
+    stock: 50,
+    rating: 4.2,
+  },
+  {
+    id: 2,
+    nombre: "Auriculares Inalámbricos",
+    descripcion:
+      "Auriculares con cancelación de ruido y batería de larga duración.",
+    precio: 59.99,
+    categoria: "Electrónica",
+    imagen:
+      "https://images.bidcom.com.ar/resize?src=https://static.bidcom.com.ar/publicacionesML/productos/ABLUE164/1000x1000-ABLUE164.jpg&h=400&q=100",
+    stock: 30,
+    rating: 4.7,
+  },
+  {
+    id: 3,
+    nombre: "Smartwatch Deportivo",
+    descripcion:
+      "Reloj inteligente con monitor de frecuencia cardiaca y seguimiento de actividad.",
+    precio: 129.99,
+    categoria: "Tecnología",
+    imagen:
+      "https://rukminim2.flixcart.com/image/850/1000/xif0q/shopsy-smartwatch/p/b/6/1-44-android-ios-t500-smart-watch-with-bluetooth-calling-black-original-imagvy24hyyqbzvm.jpeg?q=90&crop=false",
+    stock: 20,
+    rating: 4.5,
+  },
+  {
+    id: 4,
+    nombre: "Mochila Escolar",
+    descripcion:
+      "Mochila resistente y espaciosa, ideal para el colegio o la universidad.",
+    precio: 39.99,
+    categoria: "Accesorios",
+    imagen:
+      "https://images.tcdn.com.br/img/img_prod/700556/mochila_escolar_juvenil_8732253_4383_1_5672ae484f4ecb5d63b1c9ceffda2eeb.jpg",
+    stock: 40,
+    rating: 4.0,
+  },
+  {
+    id: 5,
+    nombre: "Zapatos de Running",
+    descripcion:
+      "Calzado cómodo y ligero diseñado para running y entrenamientos intensos.",
+    precio: 89.99,
+    categoria: "Calzado",
+    imagen:
+      "https://nivia.com.co/cdn/shop/files/Tenis_Ultra_Running_Azul_Nivia_Xbox_5.jpg?v=1697839355&width=1445",
+    stock: 25,
+    rating: 4.3,
+  },
+];
