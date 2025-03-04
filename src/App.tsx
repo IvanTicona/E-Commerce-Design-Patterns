a886e152b5edbc52ae48df016d8d936de7230cce
Danilepez <danilepez5@gmail.com>
2025-03-03 22:48:58 -0400

    checkout frontend

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { Route, Routes } from "react-router-dom";
+
+import IndexPage from "@/pages/index";
+import DocsPage from "@/pages/docs";
+import PricingPage from "@/pages/pricing";
+import BlogPage from "@/pages/blog";
+import AboutPage from "@/pages/about";
+import CheckoutPage from "./pages/checkout";
+
+function App() {
+  return (
+    <Routes>
+      <Route element={<IndexPage />} path="/" />
+      <Route element={<DocsPage />} path="/docs" />
+      <Route element={<PricingPage />} path="/pricing" />
+      <Route element={<BlogPage />} path="/blog" />
+      <Route element={<AboutPage />} path="/about" />
+      <Route element={<AboutPage />} path="/about" />
+      <Route element={<CheckoutPage />} path="/checkout" />
+    </Routes>
+  );
+}
+
+export default App;
